ed607331e1a4a7b4768e02d28ca2d4bb8a6902cc
MatheusGeo <geovane.santos@edu.unipar.br>
2020-06-23 21:12:57 -0300

    first Commit

--- app/src/main/java/com/geoluc/gpscapture/MapsActivity.java
+++ app/src/main/java/com/geoluc/gpscapture/MapsActivity.java
@@ -1,19 +1,38 @@
 package com.geoluc.gpscapture;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private Marker currentLocationMarker;
+    private LatLng currentLocationLatLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        startGettingLocations();
     }
 
     /**
@@ -39,8 +59,156 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng paranavai = new LatLng(-23.073995, -52.439288);
+        mMap.addMarker(new MarkerOptions().position(paranavai).title("Marcador em Paranavaí"));
+        CameraPosition cameraPosition = new CameraPosition.Builder().zoom(18).target(paranavai).build();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paranavai));
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(currentLocationMarker != null){
+            currentLocationMarker.remove();
+        }
+        // Adicionar Marcador
+        currentLocationLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(currentLocationLatLong);
+        markerOptions.title("Locaização atual");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        currentLocationMarker = mMap.addMarker(markerOptions);
+
+        //Mover a câmera para o novo local
+        CameraPosition cameraPosition = new CameraPosition.Builder().zoom(18).target(currentLocationLatLong).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        Toast.makeText(this, "Localização atualizada", Toast.LENGTH_LONG).show();
+    }
+
+    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList result = new ArrayList();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+        return result;
+    }
+
+    private boolean hasPermission(String permission) {
+        if (canAskPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canAskPermission() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("GPS desativado!");
+        alertDialog.setMessage("Ativar GPS?");
+        alertDialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    private void startGettingLocations() {
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        boolean isGPS = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetwork = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean canGetLocation = true;
+        int ALL_PERMISSIONS_RESULT = 101;
+        long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;// Distance in meters
+        long MIN_TIME_BW_UPDATES = 1000 * 10;// Time in milliseconds
+
+        ArrayList<String> permissions = new ArrayList<>();
+        ArrayList<String> permissionsToRequest;
+
+        permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        permissionsToRequest = findUnAskedPermissions(permissions);
+
+        //Check if GPS and Network are on, if not asks the user to turn on
+        if (!isGPS && !isNetwork) {
+            showSettingsAlert();
+        } else {
+            // check permissions
+
+            // check permissions for later versions
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (permissionsToRequest.size() > 0) {
+                    requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                            ALL_PERMISSIONS_RESULT);
+                    canGetLocation = false;
+                }
+            }
+        }
+
+
+        //Checks if FINE LOCATION and COARSE Location were granted
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Starts requesting location updates
+        if (canGetLocation) {
+            if (isGPS) {
+                lm.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+            } else if (isNetwork) {
+                // from Network Provider
+
+                lm.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+            }
+        } else {
+            Toast.makeText(this, "Não é possível obter a localização", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
